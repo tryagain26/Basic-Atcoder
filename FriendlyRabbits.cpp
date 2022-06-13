8dfc29361b24c4f61d3c129d4d54df1f75076e51
tryagain26 <103236793+tryagain26@users.noreply.github.com>
2022-06-13 15:46:44 +0700

    Add files via upload

---
+++ FriendlyRabbits.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+#define rep(i, a, b) for(int i = a; i < b; i++)
+#define rep1(i, a, b) for(int i = a; i <= b; i++)
+int arr[100010];
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+    int n;
+    cin >> n;
+
+    rep1(i, 1, n){
+        cin >> arr[i];
+
+    }
+    int cnt = 0;
+    rep1(i, 1, n){
+        if(arr[arr[i]] == i){
+            cnt++;
+        }
+    }
+    cout << cnt/2;
+    return 0;
+
+}
