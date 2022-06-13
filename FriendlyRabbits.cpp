975214a88fe3be98eee65aaf072d42c27c7af47b
tryagain26 <103236793+tryagain26@users.noreply.github.com>
2022-06-13 15:47:49 +0700

    Update FriendlyRabbits.cpp

--- FriendlyRabbits.cpp
+++ FriendlyRabbits.cpp
@@ -5,6 +5,8 @@ using namespace std;
 #define rep(i, a, b) for(int i = a; i < b; i++)
 #define rep1(i, a, b) for(int i = a; i <= b; i++)
 int arr[100010];
+// https://atcoder.jp/contests/code-festival-2016-quala/tasks/codefestival_2016_qualA_b?lang=en
+// O(n)
 
 int main(){
     ios_base::sync_with_stdio(false);
